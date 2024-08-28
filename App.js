43ee4d16b54f23fc74fd5ae07ebd0a11a36f385a
geronilton <geronilton001@gmail.com>
2024-08-28 15:34:08 -0300

    implementaçao da navegação em pilha

--- App.js
+++ App.js
@@ -1,29 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './screens/register/Login'
-import Cadastro from  './screens/register/Cadastro'
-
+import Cadastro from './screens/register/Cadastro'
+import Perfil from './src/components/Perfil/Perfil';
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function Menu() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Perfil" component={Perfil} />
+
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator initialRouteName='Cadastro'>
-        <Tab.Screen name="Login" component={Login} />
-        <Tab.Screen name="Cadastro" component={Cadastro} />
-      </Tab.Navigator>
+      <Stack.Navigator initialRouteName="Cadastro">
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="Cadastro" component={Cadastro} />
+        <Stack.Screen name="Menu" component={Menu} />
+
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
