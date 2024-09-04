f421c6a68b3aa574effeb239bdfbcf42122d0000
geronilton <geronilton001@gmail.com>
2024-09-04 15:44:15 -0300

    organizando

---
+++ src/screens/Perfil/Perfil.jsx
@@ -0,0 +1,59 @@
+import { Text, View, FlatList, Button, Image } from 'react-native';
+import React, { useState } from 'react';
+import styles from './style/MyStyle';
+
+const DATA = [
+    {
+        id: 1,
+        nome: 400.0,
+    },
+    {
+        id: 2,
+        nome: 200.0,
+    },
+    {
+        id: 3,
+        nome: 500.0,
+    },
+];
+
+const Item = ({ id, nome }) => {
+    return (
+        <View style={styles.tabelaid}>
+            <Text style={styles.tabdados}>{id}</Text>
+            <Text style={styles.tabdados}>{nome}</Text>
+        </View>
+    );
+};
+
+export default function Perfil() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.perfil}>
+                <View style={styles.logo}>
+                    <Image style={styles.image} source={require('../../images/ClaraOswald.jpg')} />
+                </View>
+                <View style={styles.nome}>
+                    <Text style={styles.titlename}>Clara Oswald</Text>
+                </View>  
+            </View>
+
+            <View style={styles.info}>
+                <Text style={styles.dados}>Email:</Text>
+                <Text style={styles.text}>claraoswald@gmail.com</Text>
+            </View>
+
+            <View>
+                <Text style={styles.historico}>Histórico de Consumo</Text>
+            </View>
+            <Text style={styles.historico}>Data</Text>
+            <View style={styles.tabela}>
+                <FlatList
+                    data={DATA}
+                    renderItem={({ item }) => <Item nome={item.nome} id={item.id}/>}
+                    keyExtractor={item => item.id.toString()}
+                />
+            </View>
+        </View>
+    );
+}
---
+++ src/screens/outlet/TelaTomadas.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { View, Button, Text, Image, } from "react-native";
+
+import Styles from "./Style/MyStyles_tomadas";
+
+export default function TelaTomadas( ) {
+    return(
+        <View style={Styles.container}>
+            <Text>TOMADAS</Text>
+                <View style={Styles.tomadaCadastrada}>
+                    <View style={Styles.tomadaId}>
+                    <Image
+                        style={Styles.imagemTomada}
+                        source={require('../../images/tomada.png')}
+                    />
+                        <Text>1</Text>
+                    </View>
+                    <View style={Styles.containerInfoTomada}>
+                        <Text>TV DA SALA</Text>
+                        <View style={Styles.tomadaCadastradaInfo}>
+                            <Text>WATTS</Text>
+                            <Text>KWH</Text>
+                        </View>
+                    </View>  
+                    <View style={Styles.butaoTomada}>
+                        <Button 
+                            title="OK"
+                        />
+                        <Button style={Styles.botao}
+                            title="excluir"
+                        />
+                    </View>
+                 </View>
+            </View>
+    )
+};
+
