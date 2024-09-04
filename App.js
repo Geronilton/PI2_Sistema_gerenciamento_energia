3f01a35745fe3ad26853272af0ad52a7063dc575
geronilton <geronilton001@gmail.com>
2024-09-03 21:47:55 -0300

    tela tomadas adcionado em tab navigator

--- App.js
+++ App.js
@@ -1,18 +1,11 @@
 import { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-<<<<<<< HEAD
-
-import Login from './screens/register/Login'
-import Cadastro from  './screens/register/Cadastro'
-import TelaTomadas from './screens/outlet/TelaTomadas';
-
-=======
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './screens/register/Login'
 import Cadastro from './screens/register/Cadastro'
 import Perfil from './src/components/Perfil/Perfil';
->>>>>>> ce484b94c29540a44f8774bbf3e8651f4c11d618
+import TelaTomadas from './screens/outlet/TelaTomadas';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -23,6 +16,10 @@ function Menu() {
       <Tab.Screen
         name="Perfil"
         component={Perfil}
+      />
+      <Tab.Screen
+        name="TelaTomadas"
+        component={TelaTomadas}
       />
 
     </Tab.Navigator>
@@ -35,16 +32,9 @@ export default function App() {
 
   return (
     <NavigationContainer>
-<<<<<<< HEAD
-      <Tab.Navigator initialRouteName='Cadastro'>
-        <Tab.Screen name="Login" component={Login} />
-        <Tab.Screen name="Cadastro" component={Cadastro} />
-        <Tab.Screen name="TelaTomadas" component={TelaTomadas} />
-      </Tab.Navigator>
-=======
       <Stack.Navigator initialRouteName="Cadastro">
-      
-      {!user ? (
+
+        {!user ? (
           <Stack.Screen
             name="Login"
             options={{ headerShown: false }}
@@ -66,7 +56,6 @@ export default function App() {
         />
 
       </Stack.Navigator>
->>>>>>> ce484b94c29540a44f8774bbf3e8651f4c11d618
     </NavigationContainer>
   );
 }
