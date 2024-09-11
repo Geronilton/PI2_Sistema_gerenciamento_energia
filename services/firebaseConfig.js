14db64bafd87008574e02da1455ffe6731d92d9e
geronilton <geronilton001@gmail.com>
2024-09-11 14:22:45 -0300

    projeto firebase defintivo adcionado

--- services/firebaseConfig.js
+++ services/firebaseConfig.js
@@ -4,14 +4,24 @@ import { getFirestore } from "firebase/firestore";
 import { getDatabase } from "firebase/database"; 
 
 // meu banco de dados de teste ainda adcionarei o definitivo
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAPv5LbTNEL7Lyv2nlBTQTnmirNGP0uUdc",
+//   authDomain: "gerenciadorenergia-4f6e0.firebaseapp.com",
+//   projectId: "gerenciadorenergia-4f6e0",
+//   storageBucket: "gerenciadorenergia-4f6e0.appspot.com",
+//   messagingSenderId: "1089118535787",
+//   appId: "1:1089118535787:web:befccdd55a3fff29ff312e",
+//   measurementId: "G-SV34VCFR8B",
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAPv5LbTNEL7Lyv2nlBTQTnmirNGP0uUdc",
-  authDomain: "gerenciadorenergia-4f6e0.firebaseapp.com",
-  projectId: "gerenciadorenergia-4f6e0",
-  storageBucket: "gerenciadorenergia-4f6e0.appspot.com",
-  messagingSenderId: "1089118535787",
-  appId: "1:1089118535787:web:befccdd55a3fff29ff312e",
-  measurementId: "G-SV34VCFR8B",
+  apiKey: "AIzaSyCDRlwRoWSYbNlFt9gR4DmPYKrTr1VBaAo",
+  authDomain: "sgenergia-residencial.firebaseapp.com",
+  projectId: "sgenergia-residencial",
+  storageBucket: "sgenergia-residencial.appspot.com",
+  messagingSenderId: "889899496447",
+  appId: "1:889899496447:web:898ff9385b6784b891adca",
+  measurementId: "G-DYFMNY8GMK",
 };
 
 const app = initializeApp(firebaseConfig);
